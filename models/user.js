812926f6034291c9690d96a2ed8554253fd9056b
Nick Lobanov <nk.nicolas142@gmail.com>
2020-10-16 13:57:41 +0300

    create article schema

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = mongoose.Schema({
   name: {
@@ -7,4 +8,22 @@ const userSchema = mongoose.Schema({
     maxlength: 30,
     minlength: 2,
   },
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator(email) {
+        return validator.isEmail(email);
+      },
+      message: 'Ошибка валидации email',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    select: false,
+  },
 });
+
+module.exports = mongoose.model('user', userSchema);
