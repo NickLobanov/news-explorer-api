3f61b2d5c9c0846b67db88a02dd542d39eb17ad8
Nick Lobanov <nk.nicolas142@gmail.com>
2020-10-20 12:39:14 +0300

    create post article controller

--- controllers/article.js
+++ controllers/article.js
@@ -10,3 +10,31 @@ module.exports.getArticle = (req, res, next) => {
     })
     .catch(next);
 };
+
+module.exports.postAtricle = (req, res, next) => {
+  const {
+    keyword,
+    title,
+    text,
+    date,
+    source,
+    link,
+    image,
+  } = req.body;
+  Article.create({
+    keyword,
+    title,
+    text,
+    date,
+    source,
+    link,
+    image,
+  })
+    .then((newArticle) => {
+      if (!newArticle) {
+        res.status(500).send('Ошибка');
+      }
+      res.status(200).send(newArticle);
+    })
+    .catch(next);
+};
