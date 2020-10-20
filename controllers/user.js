024ae2b11d93bd844128be2f1530772f696ea859
Nick Lobanov <nk.nicolas142@gmail.com>
2020-10-20 14:04:44 +0300

    create login user controller

--- controllers/user.js
+++ controllers/user.js
@@ -40,3 +40,15 @@ module.exports.createNewUser = (req, res, next) => {
     })
     .catch(next);
 };
+
+module.exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      if (!user) {
+        throw new UnauthorizedError('Ошибка авторизации');
+      }
+      res.send({ token: jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' }) });
+    })
+    .catch(next);
+};
