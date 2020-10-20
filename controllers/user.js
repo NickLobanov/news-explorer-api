439c778fa4b3b056c8a9c4310c6c2d6507b62adb
Nick Lobanov <nk.nicolas142@gmail.com>
2020-10-20 13:58:47 +0300

    create new user controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 module.exports.getUser = (req, res, next) => {
@@ -7,6 +9,34 @@ module.exports.getUser = (req, res, next) => {
         res.status(404).send('Нет пользователя и таким ID');
       }
       res.status(200).send(userData);
+    })
+    .catch(next);
+};
+
+module.exports.createNewUser = (req, res, next) => {
+  const {
+    email,
+    name,
+  } = req.body;
+  bcrypt.hash(req.body.password, 15)
+    .then((hash) => User.create({
+      email,
+      name,
+      password: hash,
+    }))
+    .catch((err) => {
+      if (err.code === 11000) {
+        throw new Conflict('Пользователь с таким email уже создан');
+      }
+    })
+    .then((userData) => {
+      if (!userData) {
+        throw new BadRequestError('Некорректно введены данные');
+      }
+      res.status(200).send({
+        _id: userData._id,
+        email: userData.email,
+      });
     })
     .catch(next);
 };
