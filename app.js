83b67a2666578e7ff4cd0e6edc8be39fbc66b296
Nick Lobanov <nk.nicolas142@gmail.com>
2020-10-21 18:43:32 +0300

    generate secret key

--- app.js
+++ app.js
@@ -44,15 +44,18 @@ app.use('/', articleRouter);
 
 app.use(errorLogger);
 
-// app.use((err, req, res, next) => {
-//   const { statusCode = 500, message } = err;
-//   res
-//     .status(statusCode)
-//     .send({
-//       message: statusCode === 500
-//         ? 'На сервере произошла ошибка'
-//         : message,
-//     });
-// });
+app.use(errors());
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
+  next();
+});
 
 app.listen(PORT);
